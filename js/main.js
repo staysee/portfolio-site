d988f6dd3a1f27049ba9b21635f99f6209161cbe
Stacey Sugiono <staceysugiono@gmail.com>
2016-03-01 03:32:19 -0800

    added a favicon

--- js/main.js
+++ js/main.js
@@ -1,35 +1,23 @@
 //Sticky Nav
 $(document).ready(function(){
+
+
   $(window).bind('scroll', function() {
     var navHeight = $( window ).height() - 70;
     if ($(window).scrollTop() > navHeight) {
      $('nav').addClass('fixed');
-    }
-    else {
+      $('.section').each(function(i) {
+        if ($(this).position().top <= $(window).scrollTop() - 40){
+          $('nav a.active').removeClass('active');
+          $('nav a').eq(i).addClass('active');
+        }
+      })
+    } else {
      $('nav').removeClass('fixed');
+     $('nav a.active').removeClass('active');
+     $('nav a:first').addClass('active');
     }
   });
-});
 
-
-var section = $('section');
-var nav = $('nav');
-var navHeight = nav.outerHeight();
-
-$(window).on('scroll', function () {
-  var cur_pos = $(this).scrollTop();
-
-  section.each(function() {
-    var top = $(this).offset().top - navHeight,
-        bottom = top + $(this).outerHeight();
-
-    if (cur_pos >= top && cur_pos <= bottom) {
-      nav.find('a').removeClass('active');
-      section.removeClass('active');
-
-      $(this).addClass('active');
-      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-    }
-  });
 });
 
