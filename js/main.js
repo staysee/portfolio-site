5965ef441e0df40200af38ecf0f07d483bb86764
Stacey Sugiono <staceysugiono@gmail.com>
2016-02-29 22:56:23 -0800

    fixed a tag styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
+//Sticky Nav
 $(document).ready(function(){
- $(window).bind('scroll', function() {
- var navHeight = $( window ).height() - 70;
-   if ($(window).scrollTop() > navHeight) {
+  $(window).bind('scroll', function() {
+    var navHeight = $( window ).height() - 70;
+    if ($(window).scrollTop() > navHeight) {
      $('nav').addClass('fixed');
-   }
-   else {
+    }
+    else {
      $('nav').removeClass('fixed');
-   }
+    }
+  });
 });
+
+
+var section = $('section');
+var nav = $('nav');
+var navHeight = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  section.each(function() {
+    var top = $(this).offset().top - navHeight,
+        bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      section.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
 });
+
